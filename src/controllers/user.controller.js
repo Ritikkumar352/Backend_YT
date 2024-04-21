ee09e8a1fc0e24ae107def6b7560aefab2771930
ritikkumar352 <krritik2002@gmail.com>
2024-04-21 15:56:04 +0530

    img (avtar and coverImage upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,40 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  // get user detalis from user-- required in user model
+  // validation
+  // check if already registered or not
+  // check for images , check for avtar
+  // upload them to cloudinary, avatar 
+  // create user object -- create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return result
+
+  const {fullname,email,username,password}=req.body
+  console.log(email);
+  console.log(username);
+  console.log(fullname);
+
+
 });
 
 export { registerUser, };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const registerUser = asyncHandler(async (req, res) => {
+  // res.status(200).json({
+    // message: "hello ritikkkkkk",
+  // });
+// });
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.midleware.js";
+
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([  // middleware
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1 
+        }
+
+    ]),
+    registerUser);
 // router.route("/login").post(login);
 export default router;
 

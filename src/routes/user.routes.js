80130b244fe87a3d3fb492c7c79bec98337a0fdd
ritikkumar352 <krritik2002@gmail.com>
2024-05-08 23:51:17 +0530

    solved app crashed.. all are working and the app is running

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,25 @@
 import { Router } from "express";
+import multer from "multer"; // add on 08/05/24
 import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router();
+
+// app is running after add this block of code
+
+// Multer configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Use the original file name
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// app is running after add this block of code above
+
 
 router.route("/register").post(
   // code from here is crashing this app
@@ -21,6 +39,20 @@ router.route("/register").post(
 // router.route("/login").post(login);
 
 export default router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // import {Router} from "express";
 // import { registerUser } from "../controllers/user.controller.js";
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from "cloudniary";
+import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({

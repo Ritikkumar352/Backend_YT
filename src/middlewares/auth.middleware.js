57622f43f3797f6028107c9e7879be6e310bc3e0
ritikkumar352 <krritik2002@gmail.com>
2024-05-12 00:16:08 +0530

    auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,7 @@
+import { asyncHandler } from "../utils/asyncHandler";   
+
+
+
+export const verifyJWT=asyncHandler(async(req,res,next)=>{
+    req.cookies?.accessToken || req.header("Authorization")
+})

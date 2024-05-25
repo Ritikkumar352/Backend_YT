4733e5522755c13047d8843e6161cbab1fa32cfc
ritikkumar352 <krritik2002@gmail.com>
2024-05-25 13:54:11 +0530

    update routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import multer from "multer"; // add on 08/05/24
+//import {upload} from "../middlewares/multer.midleware.js"
 import {
   loginUser,
   logoutUser,
   registerUser,
   refreshAccessToken,
   changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+// import {upload} from "../middlewares/multer.midleware.js"
+console.log("Ritik--import upload from multer in user.route");
 
 const router = Router();
 
@@ -48,11 +58,27 @@ router.route("/login").post(loginUser);
 
 // secured routes --> LOGOT
 
+//verifyJWT verifes that user is logged in or not.. perform action only if user is logged in
+
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 
-router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails); // posst will update all details .. read about patch
 
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+router.route("/C/:username").get(verifyJWT, getUserChannelProfile);
+
+//not receiving anything fro user so we are using .get
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
 
